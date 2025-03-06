22325398842eab0b723a7466a5cd5c515bf91367
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-03-05 23:25:11 -0300

    Añade middleware de caché utilizando apicache y lo integra en las rutas de categorías y productos

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const auth = require('../middleware/auth');
+const cache = require('../middleware/cache'); // Importa el middleware de caché
 
-router.get('/', categoryController.getCategories);
+
+
+
+router.get('/',cache, categoryController.getCategories);
 router.post('/', auth, categoryController.createCategory); // Solo admin
 
 module.exports = router;
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,11 +4,12 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const auth = require('../middleware/auth');
+const cache = require('../middleware/cache'); // Importa el middleware de caché
 
 router.post('/', auth, productController.createProduct); // Solo admin
-router.get('/', productController.getProducts);
-router.get('/:id', productController.getProductById);
-router.get('/category/:id', productController.getProductByCategory);
+router.get('/', cache,productController.getProducts);
+router.get('/:id', cache, productController.getProductById);
+router.get('/category/:id', cache, productController.getProductByCategory);
 router.put('/:id', auth, productController.updateProduct); // Solo admin
 router.delete('/:id', auth, productController.deleteProduct); // Solo admin
 
